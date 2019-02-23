9a884f6d6b117b6e08c24f436a531c0d813eaef4
Shayna Patel <shaynapatel@Shaynas-MacBook-Pro.local>
2019-02-23 14:05:41 -0600

    almost done

---
+++ Twitter/TweetTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetTableViewCell.swift
+//  Twitter
+//
+//  Created by Shayna Patel on 2/23/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
